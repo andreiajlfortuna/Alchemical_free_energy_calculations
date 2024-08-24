7516ae9e957cd710a492074aeac4f16ad0aed0a2
Andreia Fortuna <andreiafortuna.ajlf@gmail.com>
2024-08-24 18:21:04 +0100

    Update 04_prod_all.sh

--- simulations/04_prod_all.sh
+++ simulations/04_prod_all.sh
@@ -5,6 +5,8 @@ dir=`pwd`
 for i in $(awk '{print $1}' slurm_systems.dat)
 
 do
+
+#check how many jobs you have submitted. "s-id username" (afortuna is my username)
 n=`s-id afortuna | tail -n +3 | wc -l`
 #n=`s-tot | grep $USER | awk '{print $7}'`
 
