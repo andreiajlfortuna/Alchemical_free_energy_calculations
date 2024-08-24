5950cede9cb56d80c6766d2ed6817ef64b352b9c
Andreia Fortuna <andreiafortuna.ajlf@gmail.com>
2024-08-24 09:20:38 +0100

    Create 02_run_init_all.sh

---
+++ simulations/02_run_init_all.sh
@@ -0,0 +1,25 @@
+#! /bin/bash
+
+dir=`pwd`
+
+for i in $(awk '{print $1}' run_init_systems.dat)
+
+do
+n=`s-id afortuna | tail -n +3 | wc -l`
+#n=`s-tot | grep $USER | awk '{print $7}'`
+
+while [ $n -gt 300 ]
+
+do
+n=`s-id afortuna | tail -n +3 | wc -l`
+#n=`s-tot | grep $USER | awk '{print $7}'`
+
+sleep 2
+
+done
+
+cd ${i}
+./run_init.sh
+cd ${dir}
+
+done
