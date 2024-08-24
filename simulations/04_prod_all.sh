3b922c0c01df6903f69694256a976b45cca5ddfe
Andreia Fortuna <andreiafortuna.ajlf@gmail.com>
2024-08-24 09:22:50 +0100

    Create 04_prod_all.sh

---
+++ simulations/04_prod_all.sh
@@ -0,0 +1,27 @@
+#! /bin/bash
+
+dir=`pwd`
+
+for i in $(awk '{print $1}' slurm_systems.dat)
+
+do
+n=`s-id afortuna | tail -n +3 | wc -l`
+#n=`s-tot | grep $USER | awk '{print $7}'`
+
+while [ $n -gt 100 ]
+
+do
+n=`s-id afortuna | tail -n +3 | wc -l`
+#n=`s-tot | grep $USER | awk '{print $7}'`
+
+sleep 2
+
+done
+
+cd ${i}
+
+./slurm_MD.sh
+
+cd ${dir}
+
+done
