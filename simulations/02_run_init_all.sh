3e951318b4fe31b0e11a48ab531d3a792ec55624
Andreia Fortuna <andreiafortuna.ajlf@gmail.com>
2024-08-24 18:22:10 +0100

    Update 02_run_init_all.sh

--- simulations/02_run_init_all.sh
+++ simulations/02_run_init_all.sh
@@ -5,9 +5,12 @@ dir=`pwd`
 for i in $(awk '{print $1}' run_init_systems.dat)
 
 do
+
+#check how many jobs you have submitted. "s-id username" (afortuna is my username)
 n=`s-id afortuna | tail -n +3 | wc -l`
 #n=`s-tot | grep $USER | awk '{print $7}'`
 
+#you can change the number of jobs that you want to submit. It depends on how many processing units and colleagues you have
 while [ $n -gt 100 ]
 
 do
